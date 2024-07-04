3703a4b2c4b24dceac2d12ddc41a8c284ead218e
xianjin.seow <xianjin.seow@bytedance.com>
2024-07-05 01:19:10 +0800

    feat: update route base url to be compatible with gh pages

--- click-game/src/routes/routes.tsx
+++ click-game/src/routes/routes.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import JoinGame from "../pages/JoinGame/JoinGame";
 import ClickGame from "../pages/ClickGame/ClickGame";
+
+const basename =
+  process.env.NODE_ENV === "production" ? "/real-time-game-demo" : "";
+
 export const ROUTE_NAMES = {
-  BASE: "/",
-  CLICK_GAME: "/click-game",
+  BASE: basename + "/",
+  CLICK_GAME: basename + "/click-game",
 };
 
 const routes = createBrowserRouter([
